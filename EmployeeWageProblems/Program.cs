e51ba4c6362030e9d85e55cecf858762d7637847
SohailLuffy <sohailislammail97@gmail.com>
2022-11-13 22:02:57 +0530

    Daily Wage Part or Full Time

--- EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/Program.cs
@@ -1,29 +1,33 @@
-﻿namespace EmployeeWageProblems
-{
-    class Uc2
+﻿namespace EmployeeWageProblems 
+{ 
+    class uc3
 
     {
         static void Main(string[] args)
         {
 
-
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
                 empHrs = 8;
+
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 16;
             }
             else
             {
                 empHrs = 0;
             }
-
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage :" + empWage);
+            Console.WriteLine("Emp wage : " + empWage);
         }
     }
 }
