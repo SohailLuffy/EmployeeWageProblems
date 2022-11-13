b6e73cc73a719beb705f3cd3a28b505aa570723f
SohailLuffy <sohailislammail97@gmail.com>
2022-11-13 21:57:15 +0530

    Checking if Absent or Present

--- EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/Program.cs
@@ -1,11 +1,24 @@
-﻿namespace EmployeeWageProblems
+﻿namespace UC1CheckEmployeePresent
 {
-    class progam
+    class UC1
+
+
     {
         static void Main(string[] args)
         {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
 
-            Console.WriteLine("Welcome to Employee Wage Computation");
+            }
+            else
+            {
+                Console.WriteLine("Emolyee is absent");
+
+            }
         }
     }
 }
