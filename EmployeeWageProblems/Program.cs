ffef49c67fc980ad918e5a5838372ccc1c6fa313
SohailLuffy <sohailislammail97@gmail.com>
2022-11-13 21:53:52 +0530

    Welcome To EmployeeWage Computation

---
+++ EmployeeWageProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageProblems
+{
+    class progam
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation");
+        }
+    }
+}
